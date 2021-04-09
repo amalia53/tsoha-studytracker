881d99941cc5aa94e7b26366c76b00cc4931a9b5
Amalia Ahola <amalia.ahola@gmail.com>
2021-04-09 17:43:55 +0300

    SQL schema

---
+++ schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE students (id SERIAL PRIMARY KEY,  username TEXT,  pw TEXT);
+CREATE TABLE goals (id SERIAL PRIMARY KEY, student_id INTEGER, course_id INTEGER, goal INTEGER, studied INTEGER);
+CREATE TABLE courses (id SERIAL PRIMARY KEY, course TEXT);
+CREATE TABLE studentcourses (id SERIAL PRIMARY KEY, student_id INTEGER, course_id INTEGER);
