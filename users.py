aeca135b787613f9fc9e3b51a5dc7dc9a7d8fe2e
Amalia Ahola <amalia.ahola@gmail.com>
2021-04-23 15:09:54 +0300

    muutos users.py

--- users.py
+++ users.py
@@ -5,12 +5,13 @@ def login(username, pw):
 	result = db.session.execute(sql, {"username":username})
 	user = result.fetchone() 
 	if user == None:
-		return invalid_username
+		return "invalid_username"
 	else:
 		hash_pw = user[0]
 		if check_password_hash(hash_pw, pw):
 			session["username"] = username
 			return ok
 		else:
-			return invalid_pw
+			return "invalid_pw"
 			
+
