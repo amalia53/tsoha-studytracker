691ddcbd61c1f81d628a464a89cd50a5e231d4bd
Amalia Ahola <amalia.ahola@gmail.com>
2021-04-25 12:56:09 +0300

    typo

--- routes.py
+++ routes.py
@@ -144,7 +144,7 @@ def courses_page():
 	
 @app.route("/courseadded", methods=["POST"])
 def course_added():
-	if is_teacher(session["username"])
+	if is_teacher(session["username"]):
 		teacher.add_course(request.form["course"], session["username"])
 	else:
 		student.add_course(request.form["course"])
