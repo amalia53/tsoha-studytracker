3f9665d9779f5bd54191b1fbc085e6a73781203b
Amalia Ahola <amalia.ahola@gmail.com>
2021-04-27 11:17:55 +0300

    korjaus

--- teacher.py
+++ teacher.py
@@ -8,13 +8,13 @@ def add_teacher(name, user_id):
 	db.session.commit()
 
 def get_teacher_id(username):
-	user_id = users.get_user_id(username)
+	user_id = users.get_user_id(username)[0]
 	sql = "SELECT id FROM teachers WHERE user_id=:user_id"
 	result = db.session.execute(sql, {"user_id":user_id})
-	return result.fetchone()[0]
+	return result.fetchone()
 	
 def add_course(course, username):
-	teacher_id = get_teacher_id(username)
+	teacher_id = get_teacher_id(username)[0]
 	sql = "INSERT INTO courses (course, teacher_id) VALUES (:course, :teacher_id)"
 	db.session.execute(sql, {"course":course, "teacher_id":teacher_id})
 	db.session.commit()
