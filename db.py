b72ef46100227ca12f281f1032ab85b81132330d
Amalia Ahola <amalia.ahola@gmail.com>
2021-05-06 11:59:35 +0300

    vaihdettiin tietokantaa

--- db.py
+++ db.py
@@ -2,6 +2,7 @@ from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+# app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://user"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
