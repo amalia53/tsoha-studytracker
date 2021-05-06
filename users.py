7c7bbd7b78f77eabd68fe03092d99ca407064bf6
Amalia Ahola <amalia.ahola@gmail.com>
2021-05-06 14:02:44 +0300

    testi

--- users.py
+++ users.py
@@ -44,7 +44,7 @@ def register(username, pw, verification, role):
     sql = "SELECT username FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
-    if user == None:
+    if not user:
         if pw == verification:
             hash_pw = generate_password_hash(pw)
             sql = "INSERT INTO users (username, pw, role) VALUES (:username, :pw, :role)"
