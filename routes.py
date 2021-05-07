95120455c788ddd987771541abe8fb18a201e764
Amalia Ahola <amalia.ahola@gmail.com>
2021-05-07 16:18:40 +0300

    refaktorointia

--- routes.py
+++ routes.py
@@ -9,9 +9,6 @@ app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
-#	username = ""
-#	if session["user_id"] != None:
-#		username = users.get_username(session["user_id"])
 	return render_template("index.html")
 
 @app.route("/login", methods=["POST"])
@@ -19,9 +16,9 @@ def login():
 	login = users.login(request.form["username"], request.form["password"])
 	if login[0] == "invalid":
 		if login[1] == "username":
-			return render_template("invalid.html", invalid = "käyttäjätunnus")
+			return render_template("invalid.html", invalid="käyttäjätunnus")
 		else:
-			return render_template("invalid.html", invalid = "salasana")
+			return render_template("invalid.html", invalid="salasana")
 	else:
 		session["user_id"] = login[0]
 		if login[1] == "teacher":
@@ -49,11 +46,11 @@ def welcome_stu():
 	username = request.form["username"]
 	registeration = users.student_reg(username, request.form["password"], request.form["verification"])
 	if  registeration == "ok":
-		return render_template("welcome.html", username = username)
+		return render_template("welcome.html", username=username)
 	elif registeration == "no_match":
-		return render_template("reg_failed.html", error = "salasanat eivät täsmänneet", role = "student")
+		return render_template("reg_failed.html", error="salasanat eivät täsmänneet", role="student")
 	else:
-		return render_template("reg_failed.html", error = "käyttäjänimi on jo käytössä",  role = "student")
+		return render_template("reg_failed.html", error="käyttäjänimi on jo käytössä",  role="student")
 
 @app.route("/welcometeacher", methods=["POST"])
 def welcome_teacher():
@@ -62,11 +59,11 @@ def welcome_teacher():
 	if  registeration == "ok":
 		return render_template("welcome.html", username = username)
 	elif registeration == "no_match":
-		return render_template("reg_failed.html", error = "salasanat eivät täsmänneet",  role = "teacher")
+		return render_template("reg_failed.html", error="salasanat eivät täsmänneet",  role="teacher")
 	elif registeration == "not_authenticated":
-		return render_template("reg_failed.html", error = "väärä tunnistautumiskoodi", role = "teacher")
+		return render_template("reg_failed.html", error="väärä tunnistautumiskoodi", role="teacher")
 	else:
-		return render_template("reg_failed.html", error = "käyttäjänimi on jo käytössä", role = "teacher")
+		return render_template("reg_failed.html", error="käyttäjänimi on jo käytössä", role="teacher")
 
 @app.route("/student")
 def student_page():
@@ -76,7 +73,7 @@ def student_page():
 		studied = student.get_students_ongoing_studies(session["user_id"])
 		done = student.get_done(studentcourses, goals, studied)
 		username = users.get_username(session["user_id"])
-		return render_template("student.html", studentcourses = studentcourses, goals = goals, studied = studied, done = done, username = username)
+		return render_template("student.html", studentcourses=studentcourses, goals=goals, studied=studied, done=done, username=username)
 	else:
 		return render_template("notallowed.html")
 	
@@ -85,7 +82,7 @@ def study():
 	if session["role"] == "student":
 		courses = student.get_students_ongoing_courses(session["user_id"])
 		courses.sort()
-		return render_template("study.html", courses = courses)
+		return render_template("study.html", courses=courses)
 	else:
 		return render_template("notallowed.html")
 	
@@ -102,7 +99,7 @@ def stats():
 		studied = student.get_students_studies(session["user_id"])
 		done = student.get_done(studentcourses, goals, studied)
 		completed = student.get_completed(session["user_id"])
-		return render_template("stats.html", studentcourses = studentcourses, goals = goals, studied = studied, done = done, completed = completed)
+		return render_template("stats.html", studentcourses=studentcourses, goals=goals, studied=studied, done=done, completed=completed)
 	else:
 		return render_template("notallowed.html")
 	
@@ -115,7 +112,7 @@ def plan():
 		studied = student.get_students_ongoing_studies(session["user_id"])
 		done = student.get_done(studentcourses, goals, studied)
 		courses.sort()
-		return render_template("plan.html", courses = courses, studentcourses = studentcourses, goals = goals, studied = studied, done = done)
+		return render_template("plan.html", courses=courses, studentcourses=studentcourses, goals=goals, studied=studied, done=done)
 	else:
 		return render_template("notallowed.html")
 	
@@ -146,7 +143,7 @@ def teacher_page():
 		courses = results[0]
 		student_counts = results[1]
 		username = users.get_username(session["user_id"])
-		return render_template("teacher.html", courses = courses, student_counts = student_counts, username = username)
+		return render_template("teacher.html", courses=courses, student_counts=student_counts, username=username)
 	else:
 		return render_template("notallowed.html")
 
@@ -155,7 +152,7 @@ def grade():
 	if session["role"] == "teacher":
 		courses = teacher.get_teachers_ongoing_courses(session["user_id"])
 		courses.sort()
-		return render_template("grade.html", courses = courses)
+		return render_template("grade.html", courses=courses)
 	else:
 		return render_template("notallowed.html")
 	
@@ -171,7 +168,7 @@ def select_course():
 def grade_course():
 	if session["role"] == "teacher":
 		students = teacher.get_students_from_course(session["course"])
-		return render_template("grade_course.html", students = students)
+		return render_template("grade_course.html", students=students)
 	else:
 		return render_template("notallowed.html")
 	
@@ -195,7 +192,7 @@ def graded():
 def teacher_stats():
 	if session["role"] == "teacher":
 		results = get_stats_table(session["user_id"])
-		return render_template("teacher_stats.html", courses = results[0], student_counts = results[1], grade = results[2], studied = results[3])
+		return render_template("teacher_stats.html", courses=results[0], student_counts=results[1], grade=results[2], studied=results[3])
 	else:
 		return render_template("notallowed.html")
 
@@ -205,7 +202,7 @@ def courses_page():
 	if session["user_id"] != None:
 		courses = teacher.get_courses()
 		courses.sort()
-		return render_template("courses.html", courses = courses)
+		return render_template("courses.html", courses=courses)
 	else:
 		return render_template("notallowed.html")
 	
