06b3ed1cb109ff643d1e873a389bc16a6d7bf4ef
Amalia Ahola <amalia.ahola@gmail.com>
2021-05-07 15:21:46 +0300

    typo

--- routes.py
+++ routes.py
@@ -10,7 +10,7 @@ app.secret_key = getenv("SECRET_KEY")
 @app.route("/")
 def index():
 	username = users.get_username(session["user_id"])
-	return render_template("index.html", username)
+	return render_template("index.html", username = username)
 
 @app.route("/login", methods=["POST"])
 def login():
